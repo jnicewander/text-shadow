3b7b4f4744862bdc91d9aa389da561ccdc78a92d
jnicewander <jnicewander@live.com>
2020-02-03 18:41:00 -0500

    Merge

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
-// assign user input to variables
-
-
-function shadowSize() {
-    const size = document.getElementById("size").value;
-    const color = document.getElementById("color").value;  
-    const direction = document.getElementById("range").value;
-    let hNeg = "";
-    let vNeg = "";
-
-    let sizeResult = "";
-
-    
-    for (let i = 0; i <= size; i++) {
-        if (direction == 1){
-            hNeg = "";
-            vNeg = "";
-        } else if (direction == 2) {
-            hNeg = "-";
-            vNeg = "";
-        } else if (direction == 3) {
-            hNeg = "-";
-            vNeg = "-";
+const shadow = {
+    size: function() {
+        const size = document.getElementById('size').value;
+        return size;
+    },
+    color: function() {
+        const color = document.getElementById('color').value;
+        return color;
+    },
+    direction: function() {
+        const direction = document.getElementById('direction').value;
+        return direction; 
+    },
+    xNegative: function() {
+        if (this.direction() == 2 | this.direction() == 3) {
+            return "-";
         } else {
-            hNeg = "";
-            vNeg = "-";
-        }
-        sizeResult += `${hNeg}${i}px ${vNeg}${i}px ${color}, `;
+            return "";
+        };
+    },
+    yNegative: function() {
+        if (this.direction() == 3 | this.direction() == 4) {
+            return "-";
+        } else {
+            return "";
+        };
+    }
+};
+
+function textCTRL() {
+    let userText = document.getElementById("textCTRL").value;
+    let displayText = document.getElementById("text");
+    if (userText.length == 0) {
+        displayText.innerHTML = "Sample Text."
+    } else {
+        displayText.innerHTML = userText;
+    }
+};
+
+
+function buildShadow() {
+    // assign user inputs
+    const size = shadow.size();
+    const color = shadow.color();
+    const xNegative = shadow.xNegative();
+    const yNegative = shadow.yNegative();
+    let rawResult = "";
+
+    for (let i = 0; i <= size; i++) {
+        rawResult += `${xNegative}${i}px ${yNegative}${i}px ${color}, `;
     };
+    
+    let newResult = rawResult.substring(0, rawResult.length -2);
+   
+    // set style equal to string in newResult 
+    document.getElementById("text").style.textShadow = newResult;
     
     
     let newResult = sizeResult.substring(0, sizeResult.length -2);
     document.getElementById("text").style.textShadow = newResult;
     document.getElementById("output").innerHTML = newResult;
-    return newResult;
 };
+
